5e191352fbd2a703dbfea97c4f120670590c91bc
blairun <blairun@gmail.com>
2020-12-20 12:58:12 -0800

    point to correct DO backend

--- src/services/Api.js
+++ src/services/Api.js
@@ -5,7 +5,7 @@ export default () => {
   return axios.create({
     // baseURL: `https://localhost:9990/`,
     // baseURL: `https://192.168.1.150:9990/`,
-    baseURL: `https://financial-do-x4bpn.ondigitalocean.app`,
+    baseURL: `https://financial-do-x4bpn.ondigitalocean.app/api`,
     headers: {
       Authorization: `Bearer ${store.state.token}`,
     },
