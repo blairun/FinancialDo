c3598d8a1b098e91a3cd3c1cf185bd7d8fde9e9f
blairun <blairun@gmail.com>
2020-12-20 12:50:32 -0800

    point to live DO app

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,8 @@ import store from '@/store/index'
 export default () => {
   return axios.create({
     // baseURL: `https://localhost:9990/`,
-    baseURL: `https://192.168.1.150:9990/`,
+    // baseURL: `https://192.168.1.150:9990/`,
+    baseURL: `https://financial-do-x4bpn.ondigitalocean.app`,
     headers: {
       Authorization: `Bearer ${store.state.token}`,
     },
