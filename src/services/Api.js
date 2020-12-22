0d4e5d98369d408510f949cfbb59efe6d9beb953
blairun <blairun@gmail.com>
2020-12-21 20:52:51 -0800

    add vue app api env variable

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,9 +3,7 @@ import store from '@/store/index'
 
 export default () => {
   return axios.create({
-    // baseURL: `https://localhost:9990/`,
-    // baseURL: `https://192.168.1.150:9990/`,
-    baseURL: `https://financial-do-x4bpn.ondigitalocean.app/api`,
+    baseURL: process.env.VUE_APP_API,
     headers: {
       Authorization: `Bearer ${store.state.token}`,
     },
