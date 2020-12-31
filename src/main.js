f1a24398fadeb992e71b55e350746abd96579d14
blairun <blairun@gmail.com>
2020-12-31 14:15:29 -0800

    update/cleanup dependencies

--- src/main.js
+++ src/main.js
@@ -7,44 +7,40 @@ import { sync } from 'vuex-router-sync'
 import {
   BootstrapVue,
   BIcon,
-  BIconChevronBarUp,
-  BIconChevronBarDown,
+  // BIconChevronBarUp,
+  // BIconChevronBarDown,
   BIconExclamationTriangleFill,
   BIconCaretUpFill,
   BIconEyeFill,
   BIconEyeSlashFill,
   BIconChevronLeft,
   BIconChevronRight,
+  BIconPlus,
+  BIconDash,
 } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vuelidate from 'vuelidate'
 Vue.use(Vuelidate)
-// import VueProgressBar from 'vue-progressbar'
-
-// const progressBarOptions = {
-//   color: '#ffffff',
-//   thickness: '4px',
-// }
-
-// Vue.use(VueProgressBar, progressBarOptions)
 
 Vue.use(BootstrapVue)
 // Vue.use(BootstrapVueIcons);
 Vue.component('BIcon', BIcon)
-Vue.component('BIconChevronBarUp', BIconChevronBarUp)
-Vue.component('BIconChevronBarDown', BIconChevronBarDown)
+// Vue.component('BIconChevronBarUp', BIconChevronBarUp)
+// Vue.component('BIconChevronBarDown', BIconChevronBarDown)
 Vue.component('BIconExclamationTriangleFill', BIconExclamationTriangleFill)
 Vue.component('BIconCaretUpFill', BIconCaretUpFill)
 Vue.component('BIconEyeFill', BIconEyeFill)
 Vue.component('BIconEyeSlashFill', BIconEyeSlashFill)
 Vue.component('BIconChevronLeft', BIconChevronLeft)
 Vue.component('BIconChevronRight', BIconChevronRight)
+Vue.component('BIconPlus', BIconPlus)
+Vue.component('BIconDash', BIconDash)
 Vue.config.productionTip = false
 
 sync(store, router)
 
-// IDEA upgrade to vue 3. wait for migration tool
+// IDEA upgrade to vue 3. Wait for migration tool
 // IDEA convert to typescript app
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -21,14 +21,7 @@ module.exports = {
         options.transformAssetUrls = {
           img: 'src',
           image: 'xlink:href',
-          'b-avatar': 'src',
-          'b-img': 'src',
-          'b-img-lazy': ['src', 'blank-src'],
-          'b-card': 'img-src',
-          'b-card-img': 'src',
-          'b-card-img-lazy': ['src', 'blank-src'],
           'b-carousel-slide': 'img-src',
-          'b-embed': 'src',
         }
         return options
       })
